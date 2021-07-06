efab4bffc73150c5757b738bf0b490a6c5038f4a
Ayah <eng.aya.elsayed.2020@gmail.com>
2021-07-06 17:40:25 +0200

    Changing Text & HTML Content

---
+++ htlmlContent.js
@@ -0,0 +1,11 @@
+const books = document.querySelectorAll('#book-list li .name');
+
+Array.from(books).forEach(function (book) {
+    book.textContent += '(Book title)';
+    // book.textContent = 'test'
+});
+
+const bookList = document.querySelector('#book-list');
+// bookList.innerHTML = '<h2>Books and more books...</h2>';
+bookList.innerHTML += '<p>This is how you add HTML content</p>';
+
