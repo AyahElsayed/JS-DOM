c437e71878df60d523d3a73ce5cf038473fe5c90
Ayah <eng.aya.elsayed.2020@gmail.com>
2021-07-06 17:21:29 +0200

    The Query Selector

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const search = document.getElementById('search-books');
 const bookList = document.getElementById('book-list');
 
-console.log(search, bookList);
+// console.log(search, bookList);
 
 // -------------------
 
@@ -13,8 +13,23 @@ console.log(Array.isArray(titles)); // false
 console.log(Array.isArray(Array.from(titles))); // true
 
 Array.from(titles).forEach(function (title) {
-    console.log(title);
+    // console.log(title);
 });
 
 const lis = document.getElementsByTagName('li');
-console.log(lis)
+// console.log(lis)
+
+// -------------------------------
+
+const wmf = document.querySelector('#book-list li:nth-child(2) .name');
+// console.log(wmf);
+
+var books = document.querySelector('#book-list li .name');
+// console.log(books);
+
+books = document.querySelectorAll('#book-list li .name');
+// console.log(books);
+
+Array.from(books).forEach(function (book) {
+    // console.log(book);
+});
