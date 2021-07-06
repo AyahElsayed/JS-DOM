675440939b53c119def0d120fdef72c525ae283a
Ayah <eng.aya.elsayed.2020@gmail.com>
2021-07-06 17:10:26 +0200

    Get Elements By Class or Tag

--- app.js
+++ app.js
@@ -2,3 +2,19 @@ const search = document.getElementById('search-books');
 const bookList = document.getElementById('book-list');
 
 console.log(search, bookList);
+
+// -------------------
+
+const titles = document.getElementsByClassName('title');
+
+console.log(Array.isArray(titles)); // false
+
+// create an array from this html collection
+console.log(Array.isArray(Array.from(titles))); // true
+
+Array.from(titles).forEach(function (title) {
+    console.log(title);
+});
+
+const lis = document.getElementsByTagName('li');
+console.log(lis)
